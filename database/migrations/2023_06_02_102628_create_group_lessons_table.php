887f74110e29ba9f5d46219f1b57488b2613ab31
Lents Maxim <lents.maxim.developer@gmail.com>
2023-06-02 13:47:53 +0300

    added migrations

---
+++ database/migrations/2023_06_02_102628_create_group_lessons_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_lessons', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('group_id')->constrained('groups');
+            $table->foreignId('lesson_id')->constrained('lessons');
+            $table->double('order', 5, 4)->default(0.0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lessons');
+    }
+};
